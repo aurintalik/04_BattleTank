89478be0bba1327f6377bf227674e7894dadec95
Amanda <aurintalik@gmail.com>
2018-03-27 22:39:41 -0700

    End of day commit

--- Section_04/BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ Section_04/BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -29,7 +29,6 @@ void AProjectile::Tick(float DeltaTime)
 
 void AProjectile::LaunchProjectile(float Speed)
 {
-	UE_LOG(LogTemp, Warning, TEXT("FIRE BUTTON PRESSED! %f speed!"), Speed);
 	ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector * Speed);
 	ProjectileMovement->Activate();
 }
